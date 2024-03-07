9ddf71c08c7d62a578f0b600aa49d9e89b04d0e7
Alex Sung <gihyeok.sung@kellogg.northwestern.edu>
2024-03-07 05:54:36 +0000

    1

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,13 +4,17 @@ class EntriesController < ApplicationController
   end
 
   def create
+    @user = User.find_by({ "id" => session["user_id"] })
+    if @user != nil
     @entry = Entry.new
     @entry["title"] = params["title"]
     @entry["description"] = params["description"]
     @entry["occurred_on"] = params["occurred_on"]
     @entry["place_id"] = params["place_id"]
+    @entry["user_id"] = @user["id"]
     @entry.save
-    redirect_to "/places/#{@entry["place_id"]}"
+  else
+    flash["notice"] = "Login first."
   end
-
+  redirect_to "/places/#{@entry["place_id"]}"
 end
