d25709952bda488b33cb732083cf5ee21b7b6b4f
Alex Sung <gihyeok.sung@kellogg.northwestern.edu>
2024-03-11 03:06:39 +0000

    9

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,13 +4,26 @@ class EntriesController < ApplicationController
   end
 
   def create
-    @entry = Entry.new
-    @entry["title"] = params["title"]
-    @entry["description"] = params["description"]
-    @entry["occurred_on"] = params["occurred_on"]
-    @entry["place_id"] = params["place_id"]
-    @entry["image"] = params["image"]
-    @entry.save
+    if @current_user
+      @entry = Entry.new
+      @entry["title"] = params["title"]
+      @entry["description"] = params["description"]
+      @entry["occurred_on"] = params["occurred_on"]
+      @entry["place_id"] = params["place_id"]
+      @entry.uploaded_image.attach(params["uploaded_image"])
+      @entry.save
+    else
+      flash["notice"] = "Login first."
+    end
   redirect_to "/places"
+end
+
+  # removes security restrictions for API calls
+  before_action :allow_cors
+  def allow_cors
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
+    response.headers['Access-Control-Max-Age'] = '1728000'
   end
 end
